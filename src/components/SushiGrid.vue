8b7d51b1bb44b8badc62787bbd0e832404ea4960
hashrock <hashrock@users.noreply.github.com>
2018-03-03 01:06:22 +0900

    feat: add header

--- src/components/SushiGrid.vue
+++ src/components/SushiGrid.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="grid">
-    <svg width=500>
-      <g v-for="(row, ri) in data" :key="ri" :transform="translateRow(ri)">
-        <g v-for="(col, ci) in row" :key="ci" :transform="translateCol(ci)">
-          <rect x=0 y=0 width=100 height=24>
-          </rect>
-          <text x=0 y=12 width=100 height=24>{{col}}</text>
-        </g>
+    <svg width=500 height=400>
+      <g v-for="(col, ci) in header" :key="ci" :transform="translateCol(ci)">
+        <rect class="col-header" x=0 y=0 width=100 height=24>
+        </rect>
+        <text text-anchor="middle" x=49 y=12 width=100 height=24>{{col}}</text>
       </g>
-      <rect class="selection" x=0 y=0 width=100 height=24></rect>
+      <g transform="translate(0,24)">
+
+        <g v-for="(row, ri) in data" :key="ri" :transform="translateRow(ri)">
+          <g v-for="(col, ci) in row" :key="ci" :transform="translateCol(ci)">
+            <rect x=0 y=0 width=100 height=24 @click="selectCell(ci, ri)">
+            </rect>
+            <text x=2 y=12 width=100 height=24>{{col}}</text>
+          </g>
+        </g>
+        <rect :transform="selectionTransform" class="selection" x=0 y=0 width=100 height=24></rect>
+
+      </g>
     </svg>
   </div>
 </template>
@@ -19,8 +28,22 @@ export default {
   props: {},
   data() {
     return {
-      data: [["A1", "A2", "A3", "A4"], ["B1", "B2", "B3", "B4"]]
+      header: ["No", "Name", "Age", "Gender"],
+      data: [
+        ["A1", "A2", "A3", "A4"],
+        ["B1", "B2", "B3", "B4"],
+        ["C1", "C2", "C3", "C4"]
+      ],
+      selection: {
+        c: 1,
+        r: 0
+      }
     };
+  },
+  computed: {
+    selectionTransform() {
+      return `translate(${this.selection.c * 100},  ${this.selection.r * 24})`;
+    }
   },
   methods: {
     translateCol(ci) {
@@ -28,6 +51,10 @@ export default {
     },
     translateRow(ri) {
       return `translate(0, ${ri * 24})`;
+    },
+    selectCell(c, r) {
+      this.selection.c = c;
+      this.selection.r = r;
     }
   }
 };
@@ -37,16 +64,24 @@ export default {
 <style scoped>
 rect {
   fill: white;
-  stroke: gray;
+  stroke: #999;
   shape-rendering: crispEdges;
 }
 .selection {
   fill: none;
-  stroke: green;
-  stroke-width: 2px;
+  stroke: rgb(158, 55, 255);
+  stroke-width: 3px;
+}
+.col-header {
+  fill: #eee;
 }
 
 text {
   dominant-baseline: central;
+}
+
+svg {
+  border: 1px solid #999;
+  background: #eee;
 }
 </style>
