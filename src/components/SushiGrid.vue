4982653eb93aeb80760cb314cb194ab409e2307b
hashrock <hashrock@users.noreply.github.com>
2018-03-03 14:49:55 +0900

    feat: add multiple selection

--- src/components/SushiGrid.vue
+++ src/components/SushiGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid">
-    <svg width=500 height=400>
+    <svg width=500 height=400  @mouseup="endSelection()">
       <g v-for="(col, ci) in header" :key="ci" :transform="translateCol(ci)">
         <rect class="col-header" x=0 y=0 width=100 :height="rowHeight">
         </rect>
@@ -10,12 +10,12 @@
 
         <g v-for="(row, ri) in data" :key="ri" :transform="translateRow(ri)">
           <g v-for="(col, ci) in row" :key="ci" :transform="translateCol(ci)">
-            <rect x=0 y=0 width=100 :height="rowHeight" @click="selectCell(ci, ri)">
+            <rect x=0 y=0 width=100 :height="rowHeight" @mousedown="setSelectionStart(ci, ri)" @mousemove="setSelectionEnd(ci, ri)">
             </rect>
             <text x=2 y=12 width=100 :height="rowHeight">{{col}}</text>
           </g>
         </g>
-        <rect :transform="selectionTransform" class="selection" x=0 y=0 width=100 :height="rowHeight"></rect>
+        <rect :transform="selectionTransform" class="selection" x=0 y=0 :width="selectionSize.w * 100" :height="selectionSize.h * rowHeight"></rect>
       </g>
     </svg>
     <div class="editor__frame" :style="styleObj" v-if="editing">
@@ -41,11 +41,15 @@ export default {
       ],
       selection: {
         c: 1,
-        r: 0
+        r: 0,
+        ec: 1,
+        er: 0
       },
       editingText: "",
       editing: false,
-      rowHeight: 24
+      editingCell: { c: 0, r: 0 },
+      rowHeight: 24,
+      selectionMode: false
     };
   },
   computed: {
@@ -56,14 +60,32 @@ export default {
       };
     },
     selectionTransform() {
-      return `translate(${this.selection.c * 100},  ${this.selection.r * 24})`;
+      return `translate(${this.selectionSize.c * 100},  ${this.selectionSize.r *
+        24})`;
+    },
+    selectionSize() {
+      return {
+        r:
+          this.selection.r <= this.selection.er
+            ? this.selection.r
+            : this.selection.er,
+        c:
+          this.selection.c <= this.selection.ec
+            ? this.selection.c
+            : this.selection.ec,
+        w: Math.abs(this.selection.ec - this.selection.c) + 1,
+        h: Math.abs(this.selection.er - this.selection.r) + 1
+      };
     }
   },
   methods: {
     onBlur() {
       this.editing = false;
-
-      Vue.set(this.data[this.selection.r], this.selection.c, this.editingText);
+      Vue.set(
+        this.data[this.editingCell.r],
+        this.editingCell.c,
+        this.editingText
+      );
     },
     translateCol(ci) {
       return `translate(${ci * 100}, 0)`;
@@ -71,15 +93,35 @@ export default {
     translateRow(ri) {
       return `translate(0, ${ri * 24})`;
     },
-    selectCell(c, r) {
-      this.editingText = this.data[r][c];
-      if (this.selection.c === c && this.selection.r === r) {
-        this.editCell();
+    setSelectionStart(c, r) {
+      if (
+        this.selectionSize.c === c &&
+        this.selectionSize.r === r &&
+        this.selectionSize.w === 1 &&
+        this.selectionSize.h === 1
+      ) {
+        this.editCell(c, r);
+        return;
       }
       this.selection.c = c;
       this.selection.r = r;
+      this.selection.ec = c;
+      this.selection.er = r;
+      this.selectionMode = true;
+    },
+    setSelectionEnd(c, r) {
+      if (this.selectionMode) {
+        this.selection.ec = c;
+        this.selection.er = r;
+      }
+    },
+    endSelection() {
+      this.selectionMode = false;
     },
     editCell(c, r) {
+      this.editingCell.c = c;
+      this.editingCell.r = r;
+      this.editingText = this.data[r][c];
       this.editing = true;
       Vue.nextTick(() => {
         this.$refs.hiddenInput.focus();
@@ -114,6 +156,7 @@ rect {
 
 text {
   dominant-baseline: central;
+  user-select: none;
 }
 
 input {
